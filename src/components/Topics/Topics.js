331dfa14b5d92ec2e5312545ce468ff0e8d75ff4
Bipul-hossen <mdbipulhossain252@gmail.com>
2022-10-12 18:23:03 +0600

    add topics

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -4,9 +4,9 @@ import Cart from '../Cart/Cart';
 
 const Topics = () => {
     const { data } = useLoaderData()
-    return ( 
+    return (
         <div className='bg-blue-50 '>
-            <h2 className='text-4xl py-5 font-semibold mt-4'>Explore and answer our latest quizzes</h2>  
+            <h2 className='text-4xl py-5 font-semibold mt-4'>Explore and answer our latest quizzes</h2>
             <div className='flex flex-wrap gap-4 flex-col justify-evenly items-center py-6  md:flex-row sm:flex-row'>
                 {
                     data.map(singleQuizCategory => <Cart
@@ -18,5 +18,4 @@ const Topics = () => {
         </div>
     );
 };
-
 export default Topics;
